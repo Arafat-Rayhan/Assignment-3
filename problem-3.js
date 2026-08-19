9e13f7d1c9fe731e15a7e5665a3076ec9dc2b992
Arafat-Rayhan <rayhan2026arafat@gmail.com>
2026-08-19 16:05:43 +0600

    develope

--- problem-3.js
+++ problem-3.js
@@ -1,28 +1,30 @@
 function countHashtags(caption) {
     if(typeof caption !== 'string'){
         return `Invalid`;
+    }else if(!caption.includes('#')){
+        return {
+        hashtagCount:0,
+        longestTag :"",
+    }
     }
     let arrayOfCaption = caption.split(' ');
     let filterOfHashTag = arrayOfCaption.filter(word => word.startsWith('#'));
     let totelHashCount = filterOfHashTag.length;
-    let mostLength = [...filterOfHashTag].sort((a,b) => b.length - a.length)[0];
+    console.log(totelHashCount);
+    let allHashtag = [...filterOfHashTag].sort((a,b) => b.length - a.length);
+    let bigtag = allHashtag[0].slice(1);
+    console.log(bigtag);
     let result = {
         hashtagCount : totelHashCount,
-        longestTag : mostLength,
+        longestTag : bigtag,
     }
-    const result2 = {
-        longestTag :"",
-    }
-    if(totelHashCount === 0){
-        return result2;
-    }else{
-        return result;
-    }
+     return result;
+    
     
     
 }
 
 
 
-const caption ='sdffj fsdfj kjhfdg jdkjl lkdjhf';
-console.log(countHashtags(caption));
+// const caption ='sdffj fsdfj kjhfdg #jdkjl lkdjhf';
+// console.log(countHashtags(caption));
